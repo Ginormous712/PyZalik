373e0ba3d139d7dcfba059518979012637f5dad5
Ginormous712 <romavitaliy470@gmail.com>
2024-06-05 15:40:37 +0300

    deleted unusefull stuff from main

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-import datetime
 
-from Note.Note import Note
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    a = Note('asda', [1,2], 3)
-    a.updated_at = datetime.datetime.now()
-    print(a.updated_at)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+   pass
